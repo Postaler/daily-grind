32223166d62e01ac4eabd8ef0b93e58abbd97b5e
Andrei Novozhilov <andreinovozhilov15@gmail.com>
2024-11-20 13:49:20 -0800

    added coffeeTemplate function and coffee object

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -1,13 +1,82 @@
-myDate = new Date();
-myDay = myDate.getDay()
+/*
+daily-grind.js
 
-            switch(myDay){
+
+Rotate by Day of Week: Use HTML/CSS & JavaScript to create a single page that rotates unique colors and content for each weekday (Sunday to Saturday) into the page.  The content must include:
+
+One unique image, with appropriate and matching content in the alt tag.  
+A paragraph or two of content that describes the daily item (paragraph must include the name of the highlighted weekday)
+A unique color that supports the image and paragraph of content
+The unique color could affect the background HTML, or an element on the page for each day of the week.  All of the above must occur within one page.
+
+
+
+
+
+name - Bubble tea
+pic - an image of bubble tea
+day - Wednesday 
+alt - the data in the alt tag 
+color - a color to match the coffee 
+
+
+
+
+*/
+
+
+
+
+
+
+let myDate = new Date();
+let = myDay = myDate.getDay()
+let today = "";
+let coffee = ""; 
+
+switch(myDay){
 
     case 0:
         today = "Sunday";
+        coffee = {};
+
+    break;
+    case 1:
+        today = "Monday";
+        coffee = {};
+    
     break;
     case 2:
         today = "Tuesday";
+        coffee = {};
+
+    break;
+    case 3:
+        today = "Wednesday";
+        coffee = {
+            name: "Bubble Tea",
+            pic: "images/bubble-tea.jpg",
+            day:"Wednesday",
+            alt:"A pic of bubble tea",
+            color:"pink",
+            desc:"I like me some bubble tea"
+        };
+    
+    break;
+    case 4:
+        today = "Thursday";
+        coffee = {};
+    
+    break;
+    case 5:
+        today = "Friday";
+        coffee = {};
+    
+    break;
+    case 6:
+        today = "Saturday";
+        coffee = {};
+    
     break;
 
 
@@ -17,3 +86,26 @@ myDay = myDate.getDay()
 }
 
 alert(today);
+console.log(coffee);
+document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
+
+
+
+
+function coffeeTemplate(coffee){
+    let myReturn = `<img src="${coffee.pic}" alt="${coffee.alt}"  id="coffee" />
+            <strong class="feature">"${coffee.day}"  Coffee Special:</strong> "${coffee.day}"  daily coffee special is <strong class="feature">"${coffee.name}" </strong>,"${coffee.desc}" </p>`;
+
+    return(myReturn);
+
+
+
+
+
+
+}
+
+
+
+
+
